1a3309b96d704631c38a341d5d177b1a598cf78f
Daniel Yankiver <dyankiver@gmail.com>
2020-11-05 15:25:13 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,11 +3,12 @@
 require 'pry'
 
 def snake_it_up(string)
+  binding.pry
   if string[0] == "s"
-    "s" + "s" + "s" + "s" + "s" + "s" + "s" + "s" +  "s" + "s" + string
+  "s" * 10 + string
   else
     string
   end
 end
 
-
+snake_it_up("hello")
